b4cc31e7ed3cd523f3665b882a5719eca8e4ce2e
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2021-06-21 15:49:46 -0400

    Create Get-UNCPath.ps1

---
+++ Get-UNCPath.ps1
@@ -0,0 +1,14 @@
+function Get-UNCPath {
+	param(
+		[string]$HostName,
+        [string]$LocalPath
+        )
+
+    $NewPath = $LocalPath -replace(":","$")
+    #delete the trailing \, if found
+    if ($NewPath.EndsWith("\")) {
+        $NewPath = [Text.RegularExpressions.Regex]::Replace($NewPath, "\\$", "")
+    }
+    $Uncpath = "\\$HostName\$NewPath"
+    Return $Uncpath
+}
